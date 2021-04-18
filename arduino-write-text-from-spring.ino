e242bc00f56f624250ec077bb0fbc27c1f73186e
zmezei <mezeienator@gmail.com>
2021-04-18 15:57:42 +0200

    writing characters to screen received from Spring boot app

--- arduino-write-text-from-spring.ino
+++ arduino-write-text-from-spring.ino
@@ -1,15 +1,20 @@
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
+const byte separator = 255;
+int numberOfLetters;
 void setup() {
-  lcd.begin(16,2);
-  lcd.print("Szep napot,");
-  lcd.setCursor(0,1);
-  lcd.print("Tunde!");
-
+  Serial.begin(9600);
 }
 
 void loop() {
-
+  numberOfLetters = Serial.available() - 1;
+  if (numberOfLetters > 0) {
+    char message[numberOfLetters];
+    for (int i = 0; i < numberOfLetters; i++) {
+      message[i] = Serial.read();
+    }
+    lcd.begin(16,2);
+    lcd.print(message);
+  }
 }
